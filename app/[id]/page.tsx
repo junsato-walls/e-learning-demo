6815aac782836b01fe3c33df2fb010b616d46c4c
jun sato <j.sato@walls-inc.com>
2024-12-23 07:17:38 +0900

    subscription test

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -13,9 +13,20 @@ const getDetailLesson = async (id: number, supabase: SupabaseClient<Database>) =
     return lesson;
 }
 
+const getpremiumContent = async (id: number, supabase: SupabaseClient<Database>) => {
+    const { data: video } = await supabase
+    .from("premium_content")
+    .select("video_url")
+    .eq("id",id)
+    .single();
+    return video;
+}
+
 const LessonDetailPage = async ({ params }: { params: { id: number } }) => {
     const supabase = createServerComponentClient<Database>({ cookies });
-    const lesson = await getDetailLesson(params.id, supabase)
+    const lesson = await getDetailLesson(params.id, supabase);
+    const video = await getpremiumContent(params.id, supabase);
+    console.log(video)
     // console.log(lesson?.created_at)
     return (
         <div className="w-full max-w-3xl mx-auto py-16 px-8">
