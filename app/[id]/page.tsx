16248309d98fa01ce6c668e9e43a0fd414ec21b0
jun sato <j.sato@walls-inc.com>
2024-12-23 08:05:13 +0900

    videoid修正

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -27,13 +27,16 @@ const LessonDetailPage = async ({ params }: { params: { id: number } }) => {
     const supabase = createServerComponentClient<Database>({ cookies });
     const lesson = await getDetailLesson(params.id, supabase);
     const video = await getpremiumContent(params.id, supabase);
-    const videoid = extractYouTubeVideoId(video?.video_url as string) as string;
+    const videoUrl = video?.video_url;
+    if (!videoUrl){
+        return <div>Video not available</div>;
+    }    
+    const videoid = extractYouTubeVideoId(videoUrl) as string;    
     return (
         <div className="w-full max-w-3xl mx-auto py-16 px-8">
             <h1 className="text-3xl mb-6">{lesson?.title}</h1>
             <p className="mb-8">{lesson?.description}</p>
             <YouTubeEmbed height={400} videoid={videoid} />
-
         </div>
     )    
 }
