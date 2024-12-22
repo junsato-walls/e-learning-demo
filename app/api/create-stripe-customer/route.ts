917f2462e7f3f3730c824a9372dce8169be057e1
jun sato <j.sato@walls-inc.com>
2024-12-23 06:51:58 +0900

    patch supabase

--- app/api/create-stripe-customer/route.ts
+++ app/api/create-stripe-customer/route.ts
@@ -14,7 +14,7 @@ export async function POST(req: NextRequest) {
     }
 
     const data = await req.json()
-    const { id, email } = data;
+    const { id, email } = data.record;
     const stripe = new initStripe(process.env.STRIPE_SECRET_KEY!)
     const customer = await stripe.customers.create({
         email,
