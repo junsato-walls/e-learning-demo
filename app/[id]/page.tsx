7ed805869981f844e4ef32785beb88ad39a51510
jun sato <j.sato@walls-inc.com>
2024-12-23 07:34:16 +0900

    subscription test

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -3,7 +3,8 @@ import { createServerComponentClient, SupabaseClient } from "@supabase/auth-help
 import React from "react";
 import { cookies } from "next/headers"
 import { Database } from "@/lib/database.types";
-
+import { YouTubeEmbed } from "@next/third-parties/google"
+import { extractYouTubeVideoId } from "@/utils/extractYoutubeVideoId";
 const getDetailLesson = async (id: number, supabase: SupabaseClient<Database>) => {
     const { data: lesson } = await supabase
     .from("lesson")
@@ -26,12 +27,15 @@ const LessonDetailPage = async ({ params }: { params: { id: number } }) => {
     const supabase = createServerComponentClient<Database>({ cookies });
     const lesson = await getDetailLesson(params.id, supabase);
     const video = await getpremiumContent(params.id, supabase);
-    console.log(video)
+    const videoid = extractYouTubeVideoId(video?.video_url as string) as string;
+    // console.log(video)
     // console.log(lesson?.created_at)
     return (
         <div className="w-full max-w-3xl mx-auto py-16 px-8">
             <h1 className="text-3xl mb-6">{lesson?.title}</h1>
-            <p className="mb-8">{lesson?.description}</p>            
+            <p className="mb-8">{lesson?.description}</p>
+            <YouTubeEmbed height={400} videoid={videoid} />
+
         </div>
     )    
 }
