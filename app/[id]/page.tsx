82f0bd19b3498c72749c253ac3b6edbe928c0997
jun sato <j.sato@walls-inc.com>
2024-12-23 10:21:12 +0900

    add youtube

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -25,11 +25,18 @@ const getpremiumContent = async (id: number, supabase: SupabaseClient<Database>)
 
 const LessonDetailPage = async ({ params }: { params: { id: number } }) => {
     const supabase = createServerComponentClient<Database>({ cookies });
-    const lesson = await getDetailLesson(params.id, supabase);
-    const video = await getpremiumContent(params.id, supabase);
+    const [lesson, video] = await Promise.all([
+        await getDetailLesson(params.id, supabase),
+        await getpremiumContent(params.id, supabase)
+    ]);
     const videoUrl = video?.video_url;
     if (!videoUrl){
-        return <div>Video not available</div>;
+        return (
+        <div className="w-full max-w-3xl mx-auto py-16 px-8">
+            <h1 className="text-3xl mb-6">{lesson?.title}</h1>
+            <p className="mb-8">{lesson?.description}</p>
+            <YouTubeEmbed height={400} videoid={""} />
+        </div>)
     }    
     const videoid = extractYouTubeVideoId(videoUrl) as string;    
     return (
