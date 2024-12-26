3b900e190812e69a8ae745427648b00a57dab776
jun sato <j.sato@walls-inc.com>
2024-12-26 18:49:20 +0900

    test

--- app/api/subscription/[priceId]/route.ts
+++ app/api/subscription/[priceId]/route.ts
@@ -10,6 +10,7 @@ export async function GET(
         const supabase = createRouteHandlerClient({cookies})
         const { data } = await supabase.auth.getUser();
         const user = data.user;
+        console.log("コンソールテスト")
 
         if(!user){
             return NextResponse.json("Unauthorized",{status: 401})
@@ -24,21 +25,36 @@ export async function GET(
         const priceId = params.priceId;
         const stripe = new initStripe(process.env.STRIPE_SECRET_KEY!);
 
-        const session = await stripe.checkout.sessions.create({
-            customer:stripe_customer_data?.stripe_customer,
-            mode: "subscription",
-            payment_method_types: ["card"],
-            line_items:[{price:priceId,quantity:1}],
-            success_url: `${location.origin}/api/subscription/payment/success`,
-            cancel_url: `${location.origin}/api/subscription/payment/cancelled`
-        });
+          // リクエストのホストを取得（例: example.com）
+         const host = req.headers.get('host') || 'localhost';
+
+          // プロトコルを手動で指定（HTTPまたはHTTPS）
+         const protocol = req.headers.get('x-forwarded-proto') || 'http';
+
+          // オリジンを組み立てる
+         const origin = `${protocol}://${host}`;
+
+         console.log("Request Origin:");
+
+        //  console.log("Request Origin:", protocol);
+         http://localhost:3000/api/subscription/price_1QZ0RFRo1S8doQfhf5l1oN0t
+
+        // const session = await stripe.checkout.sessions.create({
+        //     customer:stripe_customer_data?.stripe_customer,
+        //     mode: "subscription",
+        //     payment_method_types: ["card"],
+        //     line_items:[{price:priceId,quantity:1}],
+        //     success_url: `${location.origin}/api/subscription/payment/success`,
+        //     cancel_url: `${location.origin}/api/subscription/payment/cancelled`
+        // });
         // success_url: "http://localhost:3000/payment/success",
         // cancel_url: "http://localhost:3000/payment/cancelled"
 
         console.log("リクエストパラメータ:" + params.priceId)
         console.log("カスタマーID取得:" + stripe_customer_data?.stripe_customer)
         console.log("セッション:")
-        console.log(session)
-    return NextResponse.json({id:session.id});
+        // console.log(session)
+        return NextResponse.json({id:host});
+        // return NextResponse.json({id:session.id});
 }
 
--- components/checkout/SubscriptionButton.tsx
+++ components/checkout/SubscriptionButton.tsx
@@ -8,18 +8,20 @@ const SubscriptionButton =  ({ planId }: {planId: string}) => {
     console.log("SubscriptionButton押下：")
     console.log(`${location.origin}/api/subscription/${planId}`)
     const processSubscription = async () =>{
+        // const res = await fetch(`${location.origin}/api/subscription/${planId}`);        
+        // const res = await fetch(`http://localhost:3000/api/subscription/${planId}`);        
         const res = await fetch(
-            // `http://localhost:3000/api/subscription/${planId}`
-            `${location.origin}/api/subscription/${planId}`
-        );        
+          `http://localhost:3000/api/subscription/${planId}`
+      );     
         const data = await res.json();
-        const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY!);
-        await stripe?.redirectToCheckout({sessionId: data.id});
+        // const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY!);
+        // await stripe?.redirectToCheckout({sessionId: data.id});
         console.log(data)
     };
     // const handleSiginIn = async () =>{
 
     // }
+    // `http://localhost:3000/api/subscription/${planId}`
 
   return (
     <>    
