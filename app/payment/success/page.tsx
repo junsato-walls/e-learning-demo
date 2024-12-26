77b61080a168baf23b87a71357d379521170792e
jun sato <j.sato@walls-inc.com>
2024-12-26 17:34:08 +0900

    test webhook

---
+++ app/payment/success/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Success = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default Success
---
+++ components/checkout/SubscriptionButton.tsx
@@ -0,0 +1,30 @@
+"use client"
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import React from 'react'
+import { Button } from '../ui/button'
+import { loadStripe } from '@stripe/stripe-js'
+
+const SubscriptionButton =  ({ planId }: {planId: string}) => {
+    // const supabase = createClientComponentClient();
+    const processSubscription = async () =>{
+        const res = await fetch(
+            `http://localhost:3000/api/subscription/${planId}`
+        );        
+        const data = await res.json();
+        const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY!);
+        await stripe?.redirectToCheckout({sessionId: data.id});
+        console.log(data)
+    };
+    // const handleSiginIn = async () =>{
+
+    // }
+
+  return (
+    <>    
+    <Button onClick={processSubscription}>サブスクリプション契約する</Button>
+    {/* <Button onClick={handleSiginIn}>サインイン</Button>        */}
+    </>
+  )
+}
+
+export default SubscriptionButton
