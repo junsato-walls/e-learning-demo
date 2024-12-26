b1b8e31d3932149f535b6792abd5c6cd63e4e0f9
jun sato <j.sato@walls-inc.com>
2024-12-26 21:20:22 +0900

    stripeのcheackoutAPI実行確認

--- app/api/subscription/[priceId]/route.ts
+++ app/api/subscription/[priceId]/route.ts
@@ -1,3 +1,4 @@
+import initStripe from 'stripe';
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers"
@@ -25,6 +26,19 @@ export async function GET(
         const protocol = req.headers.get('x-forwarded-proto') || 'http';
         const origin = `${protocol}://${host}`;
 
-        return NextResponse.json({id:origin});
+        const priceId = params.priceId;
+        const stripe = new initStripe(process.env.STRIPE_SECRET_KEY!);
+
+         const session = await stripe.checkout.sessions.create({
+            customer:stripe_customer_data?.stripe_customer,
+            mode: "subscription",
+            payment_method_types: ["card"],
+            line_items:[{price:priceId,quantity:1}],
+            success_url: `${origin}/api/subscription/payment/success`,
+            cancel_url: `${origin}/api/subscription/payment/cancelled`
+        });
+
+
+        return NextResponse.json({id:session.id});
 }
 
--- components/checkout/SubscriptionButton.tsx
+++ components/checkout/SubscriptionButton.tsx
@@ -1,12 +1,10 @@
 "use client"
 import React from 'react'
 import { Button } from '../ui/button'
-// import { loadStripe } from '@stripe/stripe-js'
+import { loadStripe } from '@stripe/stripe-js'
 
 const SubscriptionButton =  ({ planId }: {planId: string}) => {
     // const supabase = createClientComponentClient();
-    console.log("SubscriptionButton押下：")
-    console.log(`${location.origin}/api/subscription/${planId}`)
     const processSubscription = async () =>{
         const res = await fetch(`${location.origin}/api/subscription/${planId}`);        
         // const res = await fetch(`http://localhost:3000/api/subscription/${planId}`);        
@@ -15,8 +13,8 @@ const SubscriptionButton =  ({ planId }: {planId: string}) => {
 
       // );     
         const data = await res.json();
-        // const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY!);
-        // await stripe?.redirectToCheckout({sessionId: data.id});
+        const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY!);
+        await stripe?.redirectToCheckout({sessionId: data.id});
         console.log(data)
     };
     // const handleSiginIn = async () =>{
